7b9084223dc938c42c8e1a72ebaa8321fde4ef58
Joey Bednar <joeyb654@gmail.com>
2023-04-08 20:50:47 -0700

    add .h,Makefile support to F6, F7

--- lua/nvim/keymaps.lua
+++ lua/nvim/keymaps.lua
@@ -26,13 +26,18 @@ end})
 
 -- Compile and run C files with F5
 -- Make with F6
-vim.api.nvim_create_autocmd("FileType", { pattern = "c", callback = function()
+-- Run project with F7
+vim.api.nvim_create_autocmd("FileType", { pattern = {"c","make","cpp"}, callback = function()
     vim.api.nvim_buf_set_keymap(0,"n","<F5>",":w<CR>:!gcc -o %< % && ./%< <CR>",vim.opt)
     vim.api.nvim_buf_set_keymap(0,"i","<F5>","<Esc>:w<CR>:!gcc -o %< % && ./%< <CR>",vim.opt)
 
     local c_project_dir = os.getenv("HOME") .. "/Documents/Code/chess"
-    vim.api.nvim_buf_set_keymap(0,"n","<F6>","<Esc>:w<CR>:!(cd " .. c_project_dir .. " && make)<CR>",vim.opt)
-    vim.api.nvim_buf_set_keymap(0,"i","<F6>",":w<CR>:!(cd " .. c_project_dir .. " && make)<CR>",vim.opt)
+    local c_project_main = "main"
+    vim.api.nvim_buf_set_keymap(0,"n","<F6>",":w<CR>:!(cd " .. c_project_dir .. " && make)<CR>",vim.opt)
+    vim.api.nvim_buf_set_keymap(0,"i","<F6>","<Esc>:w<CR>:!(cd " .. c_project_dir .. " && make)<CR>",vim.opt)
+
+    vim.api.nvim_buf_set_keymap(0,"n","<F7>",":w<CR>:!(cd " .. c_project_dir .. " && ./" .. c_project_main .. ")<CR>",vim.opt)
+    vim.api.nvim_buf_set_keymap(0,"i","<F7>","<Esc>:w<CR>:!(cd " .. c_project_dir .. " && ./" .. c_project_main .. ")<CR>",vim.opt)
 end})
 
 
