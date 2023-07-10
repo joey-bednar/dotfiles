41cc3a43a9db9322bf4f8f83a29251d9b6bbd87d
Joey Bednar <joeyb654@gmail.com>
2023-07-09 22:12:13 -0700

    add fzf-native

--- .config/nvim/lua/nvim/plugins.lua
+++ .config/nvim/lua/nvim/plugins.lua
@@ -20,6 +20,7 @@ return require('packer').startup(function(use)
         -- or                            , branch = '0.1.x',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
+    use {"nvim-telescope/telescope-fzf-native.nvim", run = "make"}
 
     -- treesitter
     use({"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"})
