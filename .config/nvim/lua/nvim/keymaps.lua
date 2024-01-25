1bb4ef2f9b9c2edfa88381a0138d6a9bce66a28b
Joey Bednar <joeyb654@gmail.com>
2024-01-25 14:50:14 -0800

    add formatter using :Format, add mason binaries to path

--- .config/nvim/lua/nvim/keymaps.lua
+++ .config/nvim/lua/nvim/keymaps.lua
@@ -1,5 +1,5 @@
 -- Keybinds
-vim.keymap.set('i', '<C-c>', '<Esc>')
+vim.keymap.set("i", "<C-c>", "<Esc>")
 
 -- File explorer
 vim.keymap.set("n", "<leader>pv", ":NvimTreeToggle<CR>")
@@ -16,32 +16,59 @@ vim.keymap.set("n", "N", "Nzzzv")
 
 -- Delete/Paste without copying
 vim.keymap.set("x", "<leader>p", [["_dP]])
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 -- Sum all rows in file with :Sum
-vim.api.nvim_create_user_command("Sum",":%!awk '{print; total+=$1}END{print total}'",{})
+vim.api.nvim_create_user_command("Sum", ":%!awk '{print; total+=$1}END{print total}'", {})
+
+-- Format file with :Format
+vim.api.nvim_create_user_command("Format", ":lua vim.lsp.buf.format()", {})
 
 -- Execute file with F5
-vim.api.nvim_buf_set_keymap(0,"n","<F5>",":!chmod +x % && ./%<CR>",vim.opt)
+vim.api.nvim_buf_set_keymap(0, "n", "<F5>", ":!chmod +x % && ./%<CR>", vim.opt)
 
 -- Run python files with F5
-vim.api.nvim_create_autocmd("FileType", { pattern = "python", callback = function()
-    vim.api.nvim_buf_set_keymap(0,"n","<F5>",":update<Bar>execute '!python3 '.shellescape(@%, 1)<CR>",vim.opt)
-    vim.api.nvim_buf_set_keymap(0,"i","<F5>","<C-o>:update<Bar>execute '!python3 '.shellescape(@%, 1)<CR>",vim.opt)
-end})
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "python",
+	callback = function()
+		vim.api.nvim_buf_set_keymap(0, "n", "<F5>", ":update<Bar>execute '!python3 '.shellescape(@%, 1)<CR>", vim.opt)
+		vim.api.nvim_buf_set_keymap(
+			0,
+			"i",
+			"<F5>",
+			"<C-o>:update<Bar>execute '!python3 '.shellescape(@%, 1)<CR>",
+			vim.opt
+		)
+	end,
+})
 
 -- Compile and run C files with F5
 -- Make with F6
 -- Run project with F7
-vim.api.nvim_create_autocmd("FileType", { pattern = {"c","make","cpp"}, callback = function()
-    vim.api.nvim_buf_set_keymap(0,"n","<F5>",":w<CR>:!gcc -o %< % && ./%< <CR>",vim.opt)
-    vim.api.nvim_buf_set_keymap(0,"i","<F5>","<Esc>:w<CR>:!gcc -o %< % && ./%< <CR>",vim.opt)
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "c", "make", "cpp" },
+	callback = function()
+		vim.api.nvim_buf_set_keymap(0, "n", "<F5>", ":w<CR>:!gcc -o %< % && ./%< <CR>", vim.opt)
+		vim.api.nvim_buf_set_keymap(0, "i", "<F5>", "<Esc>:w<CR>:!gcc -o %< % && ./%< <CR>", vim.opt)
 
-    local c_project_dir = os.getenv("HOME") .. "/Documents/Code/chess"
-    local c_project_main = "main"
-    vim.api.nvim_buf_set_keymap(0,"n","<F6>",":w<CR>:!(cd " .. c_project_dir .. " && make)<CR>",vim.opt)
-    vim.api.nvim_buf_set_keymap(0,"i","<F6>","<Esc>:w<CR>:!(cd " .. c_project_dir .. " && make)<CR>",vim.opt)
+		local c_project_dir = os.getenv("HOME") .. "/Documents/Code/chess"
+		local c_project_main = "main"
+		vim.api.nvim_buf_set_keymap(0, "n", "<F6>", ":w<CR>:!(cd " .. c_project_dir .. " && make)<CR>", vim.opt)
+		vim.api.nvim_buf_set_keymap(0, "i", "<F6>", "<Esc>:w<CR>:!(cd " .. c_project_dir .. " && make)<CR>", vim.opt)
 
-    vim.api.nvim_buf_set_keymap(0,"n","<F7>",":w<CR>:!(cd " .. c_project_dir .. " && ./" .. c_project_main .. ")<CR>",vim.opt)
-    vim.api.nvim_buf_set_keymap(0,"i","<F7>","<Esc>:w<CR>:!(cd " .. c_project_dir .. " && ./" .. c_project_main .. ")<CR>",vim.opt)
-end})
+		vim.api.nvim_buf_set_keymap(
+			0,
+			"n",
+			"<F7>",
+			":w<CR>:!(cd " .. c_project_dir .. " && ./" .. c_project_main .. ")<CR>",
+			vim.opt
+		)
+		vim.api.nvim_buf_set_keymap(
+			0,
+			"i",
+			"<F7>",
+			"<Esc>:w<CR>:!(cd " .. c_project_dir .. " && ./" .. c_project_main .. ")<CR>",
+			vim.opt
+		)
+	end,
+})
--- .config/nvim/lua/nvim/null-ls.lua
+++ .config/nvim/lua/nvim/null-ls.lua
@@ -1,13 +1,31 @@
 require("mason").setup()
 require("mason-null-ls").setup({
-    ensure_installed = {
-        -- Opt to list sources here, when available in mason.
-    },
-    automatic_installation = false,
-    handlers = {},
+	ensure_installed = {
+		-- Opt to list sources here, when available in mason.
+	},
+	automatic_installation = false,
+	handlers = {},
 })
+
+--local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 require("null-ls").setup({
-    sources = {
-        -- Anything not supported by mason.
-    }
+	sources = {
+		-- Anything not supported by mason.
+	},
+	---- you can reuse a shared lspconfig on_attach callback here
+	--on_attach = function(client, bufnr)
+	--if client.supports_method("textDocument/formatting") then
+	--vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+	--vim.api.nvim_create_autocmd("BufWritePre", {
+	--group = augroup,
+	--buffer = bufnr,
+	--callback = function()
+	---- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+	---- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+	---- vim.lsp.buf.formatting_sync()
+	--vim.lsp.buf.format({ async = false })
+	--end,
+	--})
+	--end
+	--end,
 })
