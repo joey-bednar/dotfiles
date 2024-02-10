9e4aaf71ad47bb8f9b824c2902ba58045198dd6e
Joey Bednar <joeyb654@gmail.com>
2024-02-09 22:13:47 -0800

    fix formatting

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -8,8 +8,8 @@ return {
 
 		"hrsh7th/cmp-nvim-lsp", -- needed for lsp-capabilities, will remove
 
-         -- Useful status updates for LSP
-         { 'j-hui/fidget.nvim', opts = {} },
+		-- Useful status updates for LSP
+		{ "j-hui/fidget.nvim", opts = {} },
 
 		-- Additional lua configuration, makes nvim stuff amazing!
 		"folke/neodev.nvim",
