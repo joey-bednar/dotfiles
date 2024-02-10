e42eac4105e0594986811c26a6e0d68b25fd5f00
Joey Bednar <joeyb654@gmail.com>
2024-02-09 22:10:04 -0800

    add figet

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -8,9 +8,8 @@ return {
 
 		"hrsh7th/cmp-nvim-lsp", -- needed for lsp-capabilities, will remove
 
-		-- Useful status updates for LSP
-		-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-		-- { 'j-hui/fidget.nvim', opts = {} },
+         -- Useful status updates for LSP
+         { 'j-hui/fidget.nvim', opts = {} },
 
 		-- Additional lua configuration, makes nvim stuff amazing!
 		"folke/neodev.nvim",
