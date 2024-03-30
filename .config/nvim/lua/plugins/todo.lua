3352a1eaedd7587c0f2e4e6d0f0da28962b9248f
Joey Bednar <joeyb654@gmail.com>
2024-03-29 18:43:18 -0700

    add todo-comments.nvim

---
+++ .config/nvim/lua/plugins/todo.lua
@@ -0,0 +1,28 @@
+return {
+	{
+		"folke/todo-comments.nvim",
+		lazy = false,
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		event = "VimEnter",
+		config = function()
+			-- this is the same as using opts {}
+			require("todo-comments.config").setup({
+				search = {
+					args = {
+						"--color=never",
+						"--no-heading",
+						"--with-filename",
+						"--line-number",
+						"--column",
+						"--hidden",
+					},
+				},
+			})
+
+			-- search todos keymap
+			vim.keymap.set("n", "<leader>ft", ":TodoTelescope<CR>")
+		end,
+	},
+}
