4c1379f9097f98fd7d30deb8ff9de54bf0e71249
Joey Bednar <joeyb654@gmail.com>
2025-02-16 18:48:56 -0800

    add F5 for cpp files

--- .config/nvim/lua/util/make.lua
+++ .config/nvim/lua/util/make.lua
@@ -12,6 +12,12 @@ local function gitBase(dir)
 	end
 end
 
+-- returns true if current file is a cpp file
+local function is_cpp_file()
+	local filetype = vim.bo.filetype
+	return filetype == "cpp" or filetype == "c++"
+end
+
 -- return true if file exists
 local function fileExists(filename)
 	local file = io.open(filename, "r")
@@ -30,12 +36,48 @@ end
 
 -- select best guess for compiled C executable location
 local function selectExecutable(dir)
-	if fileExists(dir .. "/build/main") then
-		return "./build/main"
-	elseif fileExists(dir .. "/build/annie") then
-		return "./build/annie"
+	local paths = { "/build/main", "/build/amelia" }
+
+	for i = 1, 2 do
+		if fileExists(dir .. paths[i]) then
+			return "." .. paths[i]
+		end
 	end
+
 	return "./main"
+end
+
+-- Create keymap for running make
+local function setMakeKeymap(git_dir, run_executable)
+	local modes = { "n", "i" }
+	for i = 1, 2 do
+		vim.api.nvim_buf_set_keymap(
+			0,
+			modes[i],
+			"<F5>",
+			"<Esc>:w<CR>:!(cd " .. git_dir .. " && make && " .. run_executable .. ")<CR>",
+			{ desc = "Run make and execute in git directory." }
+		)
+	end
+end
+
+-- Create keymap for compiling a single file for C/C++
+local function setLocalRunKeymap()
+	local compiler = "gcc"
+	if is_cpp_file() then
+		compiler = "g++"
+	end
+
+	local modes = { "n", "i" }
+	for i = 1, 2 do
+		vim.api.nvim_buf_set_keymap(
+			0,
+			modes[i],
+			"<F5>",
+			"<Esc>:w<CR>:!" .. compiler .. " -o %< % && ./%< <CR>",
+			{ desc = "Compile and execute." }
+		)
+	end
 end
 
 -- Compile and run C files/projects with F5
@@ -47,37 +89,10 @@ function MakeShortcut()
 	if git_dir ~= nil then
 		if hasMakefile(git_dir) then
 			local run_executable = selectExecutable(git_dir)
-
-			vim.api.nvim_buf_set_keymap(
-				0,
-				"n",
-				"<F5>",
-				":w<CR>:!(cd " .. git_dir .. " && make && " .. run_executable .. ")<CR>",
-				{ desc = "Run make and execute in git directory." }
-			)
-			vim.api.nvim_buf_set_keymap(
-				0,
-				"i",
-				"<F5>",
-				"<Esc>:w<CR>:!(cd " .. git_dir .. " && make && " .. run_executable .. ")<CR>",
-				{ desc = "Run make and execute in git directory." }
-			)
+			setMakeKeymap(git_dir, run_executable)
 		end
 	else
-		vim.api.nvim_buf_set_keymap(
-			0,
-			"n",
-			"<F5>",
-			":w<CR>:!gcc -o %< % && ./%< <CR>",
-			{ desc = "Compile and execute." }
-		)
-		vim.api.nvim_buf_set_keymap(
-			0,
-			"i",
-			"<F5>",
-			"<Esc>:w<CR>:!gcc -o %< % && ./%< <CR>",
-			{ desc = "Compile and execute." }
-		)
+		setLocalRunKeymap()
 	end
 end
 
@@ -88,5 +103,5 @@ end
 -- print("T",gitBase("~/personal/chess_old"))
 -- print("=================")
 -- print(hasMakefile(gitBase("~/dotfiles")))
--- print(hasMakefile(gitBase("~/personal/chess_old")))
+-- print(hasMakefile(gitBase("~/personal/amelia")))
 -- print("=================")
