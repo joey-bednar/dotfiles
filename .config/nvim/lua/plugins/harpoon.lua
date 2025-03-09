6f17c4d51f2a12ac548bf4e0c1a1b3cd733f42ef
Joey Bednar <joeyb654@gmail.com>
2025-03-08 17:08:39 -0800

    update plugins, upgrade harpoon, use mini.comment

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -1,34 +1,31 @@
 return {
 	"ThePrimeagen/harpoon",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-	},
+	branch = "harpoon2",
+	dependencies = { "nvim-lua/plenary.nvim" },
 
 	config = function()
-		local mark = require("harpoon.mark")
-		local ui = require("harpoon.ui")
-		local term = require("harpoon.term")
+		local harpoon = require("harpoon")
 
-		vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Harpoon add file." })
-		vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu, { desc = "Harpoon menu." })
+		harpoon:setup()
+
+		vim.keymap.set("n", "<leader>a", function()
+			harpoon:list():add()
+		end)
+		vim.keymap.set("n", "<C-e>", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end)
 
 		vim.keymap.set("n", "<C-h>", function()
-			ui.nav_file(1)
-		end, { desc = "Harpoon file 1." })
+			harpoon:list():select(1)
+		end)
 		vim.keymap.set("n", "<C-j>", function()
-			ui.nav_file(2)
-		end, { desc = "Harpoon file 2." })
+			harpoon:list():select(2)
+		end)
 		vim.keymap.set("n", "<C-k>", function()
-			ui.nav_file(3)
-		end, { desc = "Harpoon file 3." })
+			harpoon:list():select(3)
+		end)
 		vim.keymap.set("n", "<C-l>", function()
-			ui.nav_file(4)
-		end, { desc = "Harpoon file 4." })
-		vim.keymap.set("n", "<leader>t", function()
-			term.gotoTerminal(1)
-		end, { desc = "Harpoon terminal." })
-
-		vim.api.nvim_set_keymap("t", "<C-d>", [[<C-\><C-n>]], { noremap = true, desc = "Close terminal remap." })
+			harpoon:list():select(4)
+		end)
 	end,
 }
--- TODO: migrate to harpoon2
