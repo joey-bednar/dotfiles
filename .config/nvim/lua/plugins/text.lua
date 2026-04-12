95d43248034b9a848f20a9ae8e025c81cc6673c3
Joseph Bednar <joeyb654@gmail.com>
2026-04-12 10:24:44 -0700

    update telescope/treesitter configs, remove autotag

--- .config/nvim/lua/plugins/text.lua
+++ .config/nvim/lua/plugins/text.lua
@@ -12,34 +12,6 @@ return {
 		lazy = true,
 		event = { "BufReadPre", "BufNewFile" },
 	},
-	{
-		-- close html tags
-		"windwp/nvim-ts-autotag",
-		lazy = true,
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = {
-			"hrsh7th/nvim-cmp",
-		},
-
-		config = function()
-			local autopairs = require("nvim-autopairs")
-			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-			local cmp = require("cmp")
-
-			autopairs.setup({
-				disable_filetype = { "TelescopePrompt", "vim" },
-			})
-
-			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-
-			require("nvim-treesitter.configs").setup({
-				autotag = {
-					enable = true,
-				},
-			})
-		end,
-	},
-
 	-- auto tab sizing
 	{ "tpope/vim-sleuth" },
 }
